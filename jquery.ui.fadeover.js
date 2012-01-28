04b5c9876e8af58da0bd6dbee3adf8beba17da99
Kieran Simkin <kieran@slinq.com>
2012-01-28 13:33:53 +0000

    hide the normal image when disabled to enable jquery UIs transparency to make the button feint

--- jquery.ui.fadeover.js
+++ jquery.ui.fadeover.js
@@ -133,9 +133,14 @@ $.widget( "ui.fadeover", {
 			me.current_disabled_effect.stop();
 			me.current_disabled_effect=null;
 		}
+		if (typeof(me.current_normal_disabled_effect)!='undefined' && me.current_normal_disabled_effect!==null) { 
+			me.current_normal_disabled_effect.stop();
+			me.current_normal_disabled_effect=null;
+		}
 		me.current_disabled_effect=me.disableddiv.animate({opacity: 1.0, duration: me.options.disable_duration, complete: function() { 
 				me.current_disabled_effect=null;
 		}});
+		me.current_normal_disabled_effect=me.normaldiv.animate({opacity: 0.0, duration: me.options.disable_duration});
 	},
 	_unset_disabled: function() { 
 		var me = this;
@@ -143,9 +148,14 @@ $.widget( "ui.fadeover", {
 			me.current_disabled_effect.stop();
 			me.current_disabled_effect=null;
 		}
+		if (typeof(me.current_normal_disabled_effect)!='undefined' && me.current_normal_disabled_effect!==null) { 
+			me.current_normal_disabled_effect.stop();
+			me.current_normal_disabled_effect=null;
+		}
 		me.current_disabled_effect=me.disableddiv.animate({opacity: 0.0, duration: me.options.enable_duration, complete: function() { 
 			me.current_disabled_effect=null;
 		}});
+		me.current_normal_disabled_effect=me.normaldiv.animate({opacity: 1.0, duration: me.options.disable_duration});
 	},
 	_mouseenter: function() { 
 		var me = this;
