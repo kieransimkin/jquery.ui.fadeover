e1a8a5b78241e706d965210820845077e432124a
Kieran Simkin <kieran.simkin@public-i.info>
2012-02-23 10:16:51 +0000

    rewrite casing comment

--- jquery.ui.fadeover.button.casing.js
+++ jquery.ui.fadeover.button.casing.js
@@ -1,18 +1,13 @@
 /* jQuery.ui.fadeover.button.casing.js
- *  Ver: 1.1
- *  by Kieran Simkin - http://SlinQ.com/
+ * Ver: 1.1
+ * by Kieran Simkin - http://SlinQ.com/
  *
- *  Copyright (c) 2011-2012, Kieran Simkin
- *  All rights reserved.
- *
- *  Redistribution and use, with or without modification, are permitted provided that the following condition is met:
- *
- *  -  Redistributions of this code must retain the above copyright notice, this condition and the following disclaimer.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2011-2012, Kieran Simkin
+ * All rights reserved.
  *
  *
- * This program is a wrapper, to allow you to use FadeOver in place of standard jQuery UI buttons, without having to rewrite all your code. With this JS loaded, you can call .button(), and you'll get a FadeOver button instead of the standard jQuery UI one.
+ * This program is a wrapper, to allow you to use FadeOver in place of standard jQuery UI buttons without having to rewrite all your code. With this script loaded, you can call .button(), and you'll get a FadeOver button instead of the standard jQuery UI one.
+ *
  */
 (function( $ ) {
 	$.widget("ui.button", $.extend({}, $.ui.button.prototype, $.slinq.fadeover.prototype));
